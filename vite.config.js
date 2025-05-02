06850a50c3547455ebc2c9a7256e8dcb5719f8de
Sydorov Artem <sydorovay@gmail.com>
2025-05-02 17:19:41 +0200

    add vite plugins

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: '/index.html'
+    }
+  },
+  server: {
+    proxy: {
+      '/': {
+        target: 'http://localhost', 
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/(.*)$/, '/index.html')
+      }
+    }
+  }
 })
