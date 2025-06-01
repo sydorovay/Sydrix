61d1ee01685567374b5ecf02e97c5e10c7049871
Sydorov Artem <sydorovay@gmail.com>
2025-06-01 08:37:24 +0200

    temp: save before rebase

--- src/components/Section/BenefitsSection/BenefitsSection.module.css.d.ts
+++ src/components/Section/BenefitsSection/BenefitsSection.module.css.d.ts
@@ -3,15 +3,9 @@ declare const styles: {
   readonly "benefitTitle": string;
   readonly "benefitsList": string;
   readonly "benefitsSection": string;
-  readonly "contactButton": string;
-  readonly "dark": string;
-  readonly "expandButton": string;
-  readonly "headerBox": string;
   readonly "icon": string;
-  readonly "light": string;
-  readonly "title": string;
-  readonly "itemDescription": string;
   readonly "showAllButton": string;
+  readonly "title": string;
 };
 export = styles;
 
