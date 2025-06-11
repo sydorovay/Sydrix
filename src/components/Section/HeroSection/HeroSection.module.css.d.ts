bccc8550dea077f22baffd66f1dee93ceb191553
Sydorov Artem <sydorovay@gmail.com>
2025-06-11 20:00:19 +0200

    update Hero & top bar style

--- src/components/Section/HeroSection/HeroSection.module.css.d.ts
+++ src/components/Section/HeroSection/HeroSection.module.css.d.ts
@@ -3,6 +3,8 @@ declare const styles: {
   readonly "buttonWrapper": string;
   readonly "fadeIn": string;
   readonly "header": string;
+  readonly "icon": string;
+  readonly "line": string;
   readonly "logo": string;
   readonly "menu": string;
   readonly "subtitle": string;
