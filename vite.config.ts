e7e2292d6cc401419c76c6a0cdf229d20f1f543a
Sydorov Artem <sydorovay@gmail.com>
2025-06-29 09:03:44 +0200

    unistal svgr

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { visualizer } from 'rollup-plugin-visualizer';
-import svgrPlugin from 'vite-plugin-svgr';  
 
 export default defineConfig({
   plugins: [
     react(),
-    svgrPlugin(),
     visualizer({ open: true, filename: 'dist/bundle-report.html' }),
   ],
   server: {
