f6c7e74d0041784faabd662b931d67b4817de07a
Sydorov Artem <sydorovay@gmail.com>
2025-06-30 07:08:02 +0200

    update git path

--- src/components/Section/HeroSection/HeroSection.tsx
+++ src/components/Section/HeroSection/HeroSection.tsx
@@ -8,24 +8,37 @@ interface HeroSectionProps {
 }
 
 export default function HeroSection({ theme }: HeroSectionProps) {
-  const { lang, t} = useLanguageContext();
+  const { lang, t } = useLanguageContext();
+
+  const handleContactClick = () => {
+    const contactSection = document.getElementById('contact');
+    if (contactSection) {
+      contactSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
   return (
-   <header className={`${styles.header} ${styles[theme]}`}>
-  <h1 className={styles.title}>{t('heroTitle')}</h1>
-  <div className={styles.topRow}>
-    <SydrixLogo t={t} language={lang} />
-  </div>
+    <header className={`${styles.header} ${styles[theme]}`}>
+      <h1 className={styles.title}>{t('heroTitle')}</h1>
 
-  <div className={styles.subtitleWrapper}>
-    {Array.isArray(t('heroSubtitle')) &&
-      t('heroSubtitle').map((line, idx) => (
-        <p key={idx} className={styles.line}>
-          {line}
-        </p>
-      ))}
-  </div>
+      <div className={styles.topRow}>
+        <SydrixLogo t={t} language={lang} />
+      </div>
 
-</header>
+      <div className={styles.subtitleWrapper}>
+        {Array.isArray(t('heroSubtitle')) &&
+          t('heroSubtitle').map((line, idx) => (
+            <p key={idx} className={styles.line}>
+              {line}
+            </p>
+          ))}
+      </div>
+
+      <div className={styles.buttonWrapper}>
+        <button className={styles.button} onClick={handleContactClick}>
+          {t('contactsButtonText')}
+        </button>
+      </div>
+    </header>
   );
 }
