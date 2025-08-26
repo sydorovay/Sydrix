755626dbbb1caf077ea1b39ffa70c4971347eaa3
Sydorov Artem <sydorovay@gmail.com>
2025-08-26 09:46:37 +0200

    add vercel.json

---
+++ dist/assets/FaqPage-BsTx2Z0E.js
@@ -0,0 +1,2 @@
+import{j as e}from"./react-vendor-BO6urtm3.js";import"./vendor-CNkn8rF8.js";const t=({t:r})=>e.jsxs("main",{children:[e.jsx("h1",{children:r?.faq||"FAQ"}),e.jsx("p",{children:r?.faqText||"Feel free to reach out via email or phone."})]});export{t as default};
+//# sourceMappingURL=FaqPage-BsTx2Z0E.js.map
