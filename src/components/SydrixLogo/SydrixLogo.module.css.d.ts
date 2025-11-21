cb17e064cc1428f6a47d9ef5188b59a2bacf5e24
Sydorov Artem <sydorovay@gmail.com>
2025-11-21 18:24:23 +0100

    update logo & buttons

--- src/components/SydrixLogo/SydrixLogo.module.css.d.ts
+++ src/components/SydrixLogo/SydrixLogo.module.css.d.ts
@@ -1,7 +1,9 @@
 declare const styles: {
+  readonly "big": string;
   readonly "brand": string;
   readonly "logo": string;
   readonly "mid": string;
+  readonly "midLetters": string;
   readonly "stop1": string;
   readonly "stop2": string;
   readonly "stop3": string;
--- src/components/SydrixLogo/SydrixLogo.tsx
+++ src/components/SydrixLogo/SydrixLogo.tsx
@@ -21,14 +21,11 @@ const SydrixLogo: React.FC<LogoProps> = ({ t, language }) => {
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 320 160"
+        viewBox="0 0 600 300"
         className={styles.svgLogo}
         aria-hidden="true"
         focusable="false"
-        style={{
-          pointerEvents: 'none',
-          userSelect: 'none',
-        }}
+        style={{ pointerEvents: 'none', userSelect: 'none' }}
       >
         <defs>
           <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="0%">
@@ -40,7 +37,7 @@ const SydrixLogo: React.FC<LogoProps> = ({ t, language }) => {
 
         <text
           x="50%"
-          y="25%"
+          y="35%"
           className={styles.tagline}
           fill="url(#grad)"
         >
@@ -49,19 +46,21 @@ const SydrixLogo: React.FC<LogoProps> = ({ t, language }) => {
 
         <text
           x="50%"
-          y="60%"
+          y="50%"
           className={styles.brand}
           fill="url(#grad)"
+          dominantBaseline="middle"
+          textAnchor="middle"
           style={{ filter: 'drop-shadow(2px 2px 2px rgba(0,0,0,0.3))' }}
         >
-          <tspan>S</tspan>
+          <tspan className={styles.big}>S</tspan>
           <tspan className={styles.mid}>YDRI</tspan>
-          <tspan>X</tspan>
+          <tspan className={styles.big}>X</tspan>
         </text>
 
         <text
           x="50%"
-          y="75%"
+          y="65%"
           className={styles.tagline}
           fill="url(#grad)"
         >
