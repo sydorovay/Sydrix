0d1f1a789a9620352b983a2bc7c21251b512530b
Sydorov Artem <sydorovay@gmail.com>
2025-12-12 00:15:35 +0100

    update

--- src/styles/index.css.d.ts
+++ src/styles/index.css.d.ts
@@ -1,7 +1,6 @@
 declare const styles: {
   readonly "container": string;
   readonly "dark": string;
-  readonly "divider": string;
   readonly "icon": string;
   readonly "visually-hidden": string;
 };
